faca858fb85791b764a0d3f746d4db649052549e
Mahdi Mohamadi <mm241@yahoo.com>
2018-08-08 12:52:56 +0430

    Add Tile() class to game.js

--- js/game.js
+++ js/game.js
@@ -1,6 +1,27 @@
 // window/global scope
 var cig = {};
 
+cig.Tile = (function() {
+	function Tile(number){
+		this.initialize();
+		this.number = number;
+		this.width = this.height = 80;
+
+		var shape = new createjs.Shape();
+		shape.graphics.setStrokeStyle(1);
+		shape.graphics.beginStroke("#000");
+		shape.graphics.beginFill("#efefef");
+		shape.graphics.rect(0, 0, this.width, this.height);
+
+		this.addChild(shape);
+
+		
+	}
+
+	var p = Tile.prototype = new createjs.Container();
+	return Tile
+})();
+
 cig.Game = (function() {
 	// constructor
 	function CountItGame() {
@@ -11,13 +32,8 @@ cig.Game = (function() {
 		//EaselJS Stage
 		this.stage = new createjs.Stage(this.canvas);
 
-		var shape = new createjs.Shape();
-		shape.graphics.setStrokeStyle(1);
-		shape.graphics.beginStroke("#000");
-		shape.graphics.beginFill("#efefef");
-		shape.graphics.rect(0, 0, 80, 80);
-
-		this.stage.addChild(shape);
+		var tile = new cig.Tile(10);
+		this.stage.addChild(tile);
 
 		this.stage.update();
 	}
