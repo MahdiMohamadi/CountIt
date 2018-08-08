08a5640bde06fe0874f59fb763df1e84e2d2b4a7
Mahdi Mohamadi <mm241@yahoo.com>
2018-08-08 12:40:45 +0430

    Add first rectangle

--- js/game.js
+++ js/game.js
@@ -1,15 +1,23 @@
 // window/global scope
 var cig = {};
 
-cig.Game = (function(){
+cig.Game = (function() {
 	// constructor
 	function CountItGame() {
 		console.log("Game starts.");
 
-		this.canvas = document.getElementById('game-canvas');
+		this.canvas = document.getElementById("game-canvas");
 
 		//EaselJS Stage
 		this.stage = new createjs.Stage(this.canvas);
+
+		var shape = new createjs.Shape();
+		shape.graphics.setStrokeStyle(1);
+		shape.graphics.beginStroke("#000");
+		shape.graphics.beginFill("#efefef");
+		shape.graphics.rect(0, 0, 80, 80);
+
+		this.stage.addChild(shape);
 
 		this.stage.update();
 	}
